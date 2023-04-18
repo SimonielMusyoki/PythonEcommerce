29801889e5330e4b55ec56f8bfa6606bd4c47fdb
Simoniel Musyoki <musyoki.mtk3@gmail.com>
2023-04-18 16:02:55 +0300

    Products

---
+++ apps/catalog/admin/products.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from apps.catalog.models import Product, ProductFile
+
+
+class ProductFileInline(admin.TabularInline):
+    model = ProductFile
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "slug", "is_digital"]
+    inlines = [ProductFileInline]
+
+    class Meta:
+        model = Product
+
+
+admin.site.register(Product, ProductAdmin)
---
+++ apps/catalog/models/products.py
@@ -0,0 +1,149 @@
+import random
+import os
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+from django.db import models
+from django.db.models import Q
+from django.db.models.signals import pre_save, post_save
+from django.urls import reverse
+
+from core.utils import unique_slug_generator, get_filename
+
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    new_filename = random.randint(1, 3910209312)
+    _, ext = get_filename_ext(filename)
+    final_filename = "{new_filename}{ext}".format(new_filename=new_filename, ext=ext)
+    return f"products/{instance.pk}/{final_filename}"
+
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
+    def search(self, query):
+        lookups = (
+            Q(title__icontains=query)
+            | Q(description__icontains=query)
+            | Q(price__icontains=query)
+            | Q(tag__title__icontains=query)
+        )
+        # tshirt, t-shirt, t shirt, red, green, blue,
+        return self.filter(lookups).distinct()
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):  # Product.objects.featured()
+        return self.get_queryset().featured()
+
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id)  # Product.objects == self.get_queryset()
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
+    def search(self, query):
+        return self.get_queryset().active().search(query)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+    image = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_digital = models.BooleanField(default=False)  # User Library
+
+    objects = ProductManager()
+
+    def get_absolute_url(self):
+        # return "/products/{slug}/".format(slug=self.slug)
+        return reverse("products:detail", kwargs={"slug": self.slug})
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def name(self):
+        return self.title
+
+    def get_downloads(self):
+        qs = self.productfile_set.all()
+        return qs
+
+
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(product_pre_save_receiver, sender=Product)
+
+
+def upload_product_file_loc(instance, filename):
+    slug = instance.product.slug
+    # id_ = 0
+    id_ = instance.id
+    if id_ is None:
+        Klass = instance.__class__
+        qs = Klass.objects.all().order_by("-pk")
+        if qs.exists():
+            id_ = qs.first().id + 1
+        else:
+            id_ = 0
+    if not slug:
+        slug = unique_slug_generator(instance.product)
+    location = "product/{slug}/{id}/".format(slug=slug, id=id_)
+    return location + filename  # "path/to/filename.mp4"
+
+
+class ProductFile(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, null=True, blank=True)
+    product_file = models.FileField(
+        upload_to=upload_product_file_loc,
+        storage=FileSystemStorage(location=settings.PROTECTED_ROOT),
+    )  # path
+    filepath = models.TextField()  # '/protected/path/to/the/file/myfile.mp3'
+    free = models.BooleanField(default=False)  # purchase required
+    user_required = models.BooleanField(default=False)  # user doesn't matter
+
+    def __str__(self):
+        return str(self.file.name)
+
+    @property
+    def display_name(self):
+        og_name = get_filename(self.file.name)
+        if self.name:
+            return self.name
+        return og_name
+
+    def get_default_url(self):
+        return self.product.get_absolute_url()
+
+    def get_download_url(self):  # detail view
+        return reverse(
+            "products:download", kwargs={"slug": self.product.slug, "pk": self.pk}
+        )
